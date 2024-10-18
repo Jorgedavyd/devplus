451391bea944c1f19ee22a6371571e9e57d7b961
Jorgedavyd <jorged.encyso@gmail.com>
2024-10-18 08:27:12 -0300

    setting up the scanner

--- lua/devplus/tasks/scanner.lua
+++ lua/devplus/tasks/scanner.lua
@@ -1,4 +1,18 @@
-local treesitter = require("treesitter")
-local M = {}
+--local categories = require("devplus.setup").config.tasks.categories
+local categories = {'TODO', 'DATA'}
 
-return M
+-- TODO hola
+-- DATA hola
+
+local language = vim.bo.filetype
+local new_categories = vim.iter(categories):map(function (value)
+    return ("\"%s\""):format(value)
+end)
+
+local query = ([[
+    (((comment) @comment)
+        (#any-match? @comment %s))
+    ]]):format(new_categories:join(" "))
+
+local parsed_query = vim.treesitter.query.parse(language, query)
+
